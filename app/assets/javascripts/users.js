4eb9ba8be6c9c7192b99b20d1f702f477fb78e12
Christina <christina.v.stewart@gmail.com>
2013-08-21 01:09:04 -0700

    commented my users.js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,12 +11,17 @@ $(document).ready(function(){
 
   // setInterval(refresh_stocks_table, 1000);
 
+
+  // hash with each stock's symbol as the key and the data from the JSON response as the value (an array)
   var stock_hash = { }
 
+  // make an empty hash with the stock symbols as keys (which I got from the checkboxes)
   $('.symbul').each(function() {
     stock_hash[$(this).text()] = [];
   });
 
+  //add each bit of stock data (price at a given time) as a new item in the array which pertains to that stock
+  //create a div with that stock's symbol and append a line graph to it
   var create_graph = function(stock_data){
     stock_hash[stock_data['symbol']].push(stock_data);
     console.log(stock_hash)
@@ -31,7 +36,7 @@ $(document).ready(function(){
     });
   };
 
-
+  //send the stock info so it can interact with the db and the Stock model methods so we can get the response
   var send_ajax = function(symbol){
     $.ajax({
       type: "GET",
@@ -40,12 +45,13 @@ $(document).ready(function(){
     }).done(create_graph);
   };
 
-
+  // display the graph the first time a stock checkbox is checked
   $('.checkbox').on('change', function() {
     var symbol = $(this).find('input').attr('name');
     send_ajax(symbol);
   });
 
+  //get new stock data for whatever's checked every 5 seconds
   setInterval(function() {
     $('#morris').empty();
     $("input:checked").each(function() {
